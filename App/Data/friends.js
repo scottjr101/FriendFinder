8d65c9bd21480ba26f9c72766a66238b45e82ade
Scott Ladd <scottjr101@gmail.com>
2019-11-15 16:56:39 -0500

    got site working

---
+++ App/Data/friends.js
@@ -0,0 +1,32 @@
+var friendArray = [{
+    "name": "Rick Sanchez",
+    "photo": 
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.f9gNSyljSlXbZMqmh7hZqgHaHw%26pid%3DApi&f=1",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+},
+{
+    "name": "Jerry Smith",
+    "photo": 
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F738078769920020481%2FxpW4r-Tr_400x400.jpg&f=1&nofb=1",
+    "scores": [2, 4, 1, 5, 1, 1, 5, 5, 2, 7]
+},
+{
+    "name": "Tammy Gueterman",
+    "photo": 
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatici.behindthevoiceactors.com%2Fbehindthevoiceactors%2F_img%2Fchars%2Ftammy-rick-and-morty-10.4.jpg&f=1&nofb=1",
+    "scores": [3, 5, 2, 1, 4, 2, 2, 2, 5, 1]
+},
+{
+    "name": "Bird Person",
+    "photo": 
+    "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffanaru.com%2Frick-and-morty%2Fimage%2F257285-rick-and-morty-bird-person-rick-and-morty.jpg&f=1&nofb=1",
+    "scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+},
+{
+    "name": "Morty Smith",
+    "photo": 
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.comicvine.com%2Fuploads%2Fscale_small%2F6%2F66303%2F4469088-tumblr_inline_n0aleph3fl1r8rr6o.jpg&f=1&nofb=1",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}];
+
+module.exports = friendArray;
---
+++ App/Routing/apiRoutes.js
@@ -0,0 +1,40 @@
+var friendData = require("../data/friends");
+
+function compareFriends(s1, s2) {
+    var difference = 0;
+    for (var i = 0; i < s1.length; i++) {
+        difference += Math.abs(s1[i] - s2[i]);
+    }
+    return difference;
+}
+
+//Routing
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+
+        var newFriend = req.body;
+        var bestScore = 9999;
+        var bestMatch;
+
+        //Compare friends
+        for (var i = 0; i < friendData.length; i++) {
+            var score = compareFriends(newFriend.scores, friendData[i].scores);
+            console.log("Score for " + friendData[i].name + ": " + score);
+            if (score < bestScore) {
+                bestMatch = friendData[i];
+                bestScore = score;
+            }
+        }
+
+        console.log("Best Match:");
+        console.log(bestMatch);
+
+        // Return best match
+        friendData.push(newFriend);
+        res.json(bestMatch);
+    });
+};
