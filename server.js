c3b7f4efa4838570752747228cc0a98012cf5fa5
Scott Ladd <scottjr101@gmail.com>
2019-11-18 21:45:22 -0500

    re installed node modules again

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ var express = require("express");
 
 // Tells node that we are creating an "express" server
 var app = express();
-app.listen(process.env.PORT);
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
